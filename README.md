5ee03027d70e1652fab462d7652a01de9044f1d9
Diana L Duran <dduran.andrade@gmail.com>
2021-10-16 10:00:02 -0500

    completed cycle-0

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Study Saturdays: Week 3 - React-Redux
 
 ## **Objective:** Build an app to display students and their test scores
+
 - [ ] **Cycle 0:** Build the store ⭐️
 - [ ] **Cycle 1:** `connect` the React component
 - [ ] **Cycle 2:** Set up React Router for a "single student" view
@@ -10,11 +11,11 @@
 ## Steps
 
 - [ ] Build out the Redux store, including:
-  - [ ] Action type
-  - [ ] Action creator
-  - [ ] Thunk creator
-  - [ ] Initial state
-  - [ ] Reducer
+  - [ x] Action type
+  - [ x] Action creator
+  - [ x] Thunk creator
+  - [ x] Initial state
+  - [ x] Reducer
   - NOTE: See the dummy data in `components/StudentList.js` for an example of how the data will be structured
 - [ ] Test your Redux store functionality
   - Dispatch a few actions to your reducer, then check the logs in your console. Is it doing what you expect?
--- client/redux/store.js
+++ client/redux/store.js
@@ -1,30 +1,74 @@
-import {createStore, applyMiddleware} from 'redux';
-import loggerMiddleware from 'redux-logger';
-import axios from 'axios';
-import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from "redux";
+import loggerMiddleware from "redux-logger";
+import axios from "axios";
+import thunkMiddleware from "redux-thunk";
 
 // ACTION TYPES go here:
-
+//constants
+const FETCH_STUDENTS = "FETCH_STUDENTS";
+const FETCH_TESTS = "FETCH_TESTS";
 
 // ACTION CREATORS go here:
+const gotStudents = (students) => ({
+  type: FETCH_STUDENTS,
+  students,
+});
 
+const gotTests = (tests) => ({
+  type: FETCH_TESTS,
+  tests,
+});
 
 // THUNK CREATORS go here:
+export const getStudents = () => {
+  return async (dispatch) => {
+    // const { data } = await axios.get("/api/students");
 
+    dispatch(gotStudents());
+  };
+};
 
-const initialState = {};
+export const getTests = () => {
+  return async (dispatch) => {
+    const { data } = await axios.get("/api/tests");
+    dispatch(gotTests(data));
+  };
+};
+
+const initialState = {
+  students: [],
+  tests: [],
+};
 
 const reducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
+    case FETCH_STUDENTS:
+      return { ...state, students: action.students };
+    case FETCH_TESTS:
+      return { ...state, tests: action.tests };
     default:
       return state;
   }
-}
+};
 
-const store = createStore(reducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
+const store = createStore(
+  reducer,
+  applyMiddleware(thunkMiddleware, loggerMiddleware)
+);
 
 // dispatch your own actions here to test your store functionality:
-store.dispatch({type: 'HELLO_WORLD'})
+store.dispatch({
+  type: FETCH_STUDENTS,
+  students: [
+    {
+      id: 1,
+      fullName: "Jordan Walke",
+      firstName: "Jordan",
+      lastName: "Walke",
+      email: "jw@react.com",
+    },
+  ],
+});
 
 // ^ you can see the logs above in your console, thanks to redux-logger!
 
